a71ed0df889c3a7316bbbffa0c76e65e49f55c5a
king89 <liangqz89@gmail.com>
2015-08-12 23:39:15 -0400

    add page setting switch

--- MangaViewer/app/src/main/java/com/king/mangaviewer/actviity/MangaPageActivity.java
+++ MangaViewer/app/src/main/java/com/king/mangaviewer/actviity/MangaPageActivity.java
@@ -2,6 +2,11 @@ package com.king.mangaviewer.actviity;
 
 import android.os.Bundle;
 import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
 
 import com.king.mangaviewer.R;
 import com.king.mangaviewer.common.component.MyViewFlipper;
@@ -52,4 +57,42 @@ public class MangaPageActivity extends BaseActivity {
         return true;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.page_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_setting) {
+
+            View v = findViewById(R.id.menu_setting);
+            displayPopupWindow(v);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void displayPopupWindow(View anchorView) {
+        PopupWindow popup = new PopupWindow(this);
+        View layout = getLayoutInflater().inflate(R.layout.menu_page_setting, null);
+        popup.setContentView(layout);
+        // Set content width and height
+        popup.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+        popup.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
+        // Closes the popup window when touch outside of it - when looses focus
+        popup.setOutsideTouchable(true);
+        popup.setFocusable(true);
+        // Show anchored to button
+        popup.showAsDropDown(anchorView);
+    }
 }
